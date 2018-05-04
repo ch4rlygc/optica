4d79e9d831cc21738c060fa6a0a94d97bc52664e
aflores <alexflores09.06.90@gmail.com>
2018-05-03 21:00:44 -0600

    Email
    Implementation of sending email with the use of the template

--- mail/sendMessage.php
+++ mail/sendMessage.php
@@ -24,8 +24,34 @@ if(!empty($_POST)){
 		);
 
 		if ($response != null && $response->success) {
-			$arr = array("status" => "ok", "message" => "Correo enviado correctamente");
-			print json_encode($arr);
+
+			// Check for empty fields
+			if(empty($_POST['name'])  		||
+				empty($_POST['email']) 		||
+				empty($_POST['message'])){
+				$arr = array("status" => "fail", "message" => "Todos los campos son obligatorios");
+				print json_encode($arr);
+				die;
+			}
+
+			$name = $_POST['name'];
+			$email_address = $_POST['email'];
+			$message = $_POST['message'];
+
+			// Create the email and send the message
+			$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+			$email_subject = "Website Contact Form:  $name";
+			$email_body = "Recibió un nuevo mensaje del formulario de contacto de su sitio web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nCorreo: $email_address\n\nMensaje:\n$message";
+			$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+			$headers .= "Reply-To: $email_address";
+			if(mail($to,$email_subject,$email_body,$headers)){
+				$arr = array("status" => "ok", "message" => "Correo enviado correctamente");
+				print json_encode($arr);
+			}
+			else{
+				$arr = array("status" => "fail", "message" => "Hubo un problema al enviar el correo, intente de nuevo.");
+				print json_encode($arr);
+			}
 		}
 		else{
 			$arr = array("status" => "fail", "message" => "No se pudo enviar el mensaje");
